fa396346773cd5f66c449b81c46e2dd18c741b54
pyheejin <pyheejin@gmail.com>
2025-03-19 11:33:31 +0900

    ** 20250319 [branch:develop] 업데이트 내용
    - 티켓 구매내역 api 연동

--- lib/screens/ticket_history_screen.dart
+++ lib/screens/ticket_history_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dancemate_app/provider/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +7,8 @@ class TicketHistoryScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final ticketData = ref.watch(getUserTicketProvider(0));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('티켓 구매내역'),
@@ -32,72 +35,96 @@ class TicketHistoryScreen extends ConsumerWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('2025-03-01'),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-              ),
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
+            ticketData.when(
+              loading: () => const CircularProgressIndicator(),
+              error: (error, stack) {
+                print(error);
+                return SizedBox(
+                  width: 300,
+                  child: Text('error: $error'),
+                );
+              },
+              data: (dataList) => ListView.builder(
                 shrinkWrap: true,
-                itemCount: 3,
+                itemCount: dataList['tickets'].length,
                 itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 5,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color(0xff6555FF),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(
+                  final date = dataList['tickets'][index]['date'];
+                  final ticketList = dataList['tickets'][index]['ticket_list'];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 5,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(date),
+                        const SizedBox(height: 5),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: ticketList.length,
+                          itemBuilder: (context, index) {
+                            final nickname = ticketList[index]['nickname'];
+                            final count = ticketList[index]['count'];
+                            final price = ticketList[index]['price'];
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
                                 vertical: 5,
-                                horizontal: 15,
                               ),
-                              child: Text(
-                                'dancerNickname',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff6555FF),
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 5,
+                                        horizontal: 15,
+                                      ),
+                                      child: Text(
+                                        nickname,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    count,
+                                    style: const TextStyle(
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff6555FF),
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 5,
+                                        horizontal: 30,
+                                      ),
+                                      child: Text(
+                                        price,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ),
-                          const Text(
-                            '3회권',
-                            style: TextStyle(
-                              fontSize: 17,
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color(0xff6555FF),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(
-                                vertical: 5,
-                                horizontal: 30,
-                              ),
-                              child: Text(
-                                '128,000',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   );
                 },
